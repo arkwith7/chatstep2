cd27d0ef8c22ad5bef62f4ae373ba908c6ef8aa8
arkwith7 <arkwith7@gmail.com>
2020-09-10 18:15:00 +0900

    init

--- mychatsite/chatapp/views.py
+++ mychatsite/chatapp/views.py
@@ -1,7 +1,70 @@
-from django.shortcuts import render
+import os
+import random
+import json
+
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+from django.http import JsonResponse
+from django.core.mail import send_mail
+from smtplib import SMTPException
+
 
 # Create your views here.
- 
+context = {}
+
+import logging
+
+logger = logging.getLogger(__name__)
+work_dir = os.path.dirname(os.path.realpath(__file__))
+
+# def index(request):
+#     msg = '박형식 홈페이지'
+#     return render(request, 'chatapp/index.html', {'message': msg})
+
 def index(request):
-    return HttpResponse("<h1>Hello, World!</h1>")
+    template = loader.get_template('chatapp/base_contents_kr.html')
+#    template = loader.get_template('base_contents.html')
+    context = {
+#         'login_success' : False,
+#         'latest_question_list': "test",
+    }
+    return HttpResponse(template.render(context, request))
+
+def chat_home(request):
+    template = loader.get_template('chatapp/chat_home_screen.html')
+    context = {
+        'login_success' : False,
+        'initMessages' : ["아크위드 채팅 홈페이지에 오신것을 환영합니다",
+                          "아크위드 홈페이지 설명 챗봇이 회사의  vision, mission, 제품, 서비스, 주요기술, 연락처에 대해 답변합니다."]
+    }
+    return HttpResponse(template.render(context, request))
+
+def popup_chat_home(request):
+    template = loader.get_template('chatapp/popup_chat_home_screen.html')
+    context = {
+        'login_success' : False,
+        'initMessages' : ["아크위드 채팅 홈페이지에 오신것을 환영합니다",
+                          "아크위드 홈페이지 설명 챗봇이 회사의  vision, mission, 제품, 서비스, 주요기술, 연락처에 대해 답변합니다."]
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def call_chatbot(request):
+    if request.method == 'POST':
+        if request.is_ajax():
+            userID = request.POST['user']
+            sentence = request.POST['message']
+            #python은 unicode만을 허용한다
+            #bytesThing = sentence.encode(encoding='UTF-8')
+            #newStringThing = bytesThing.decode(encoding='UTF-8')
+            logger.debug("question[{}]".format(sentence))
+#            answer = clean_up_sentence(sentence)
+#             answer = bot.response(sentence, userID)
+            answer = bot.get_answer(sentence, userID)
+            #bytesThing = answer.encode(encoding='UTF-8')
+            #newStringThing = bytesThing.decode(encoding='UTF-8')
+            logger.debug("answer[{}]".format(answer))
+            return HttpResponse(answer)
+     
+    return render(request)
--- mychatsite/mychatsite/settings.py
+++ mychatsite/mychatsite/settings.py
@@ -55,7 +55,8 @@ ROOT_URLCONF = 'mychatsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ os.path.join(BASE_DIR, 'templates') ],  # 추가
+        # 'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,5 +118,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = ''
 
 STATIC_URL = '/static/'
